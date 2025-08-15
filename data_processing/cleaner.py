75c0a1fefd601769726f78e52bc5a29e409e1445
iZarrios <esalama057@gmail.com>
2025-08-15 16:03:42 +0300

    add data cleaning placeholder

---
+++ data_processing/cleaner.py
@@ -0,0 +1,13 @@
+### The following can be used to clean the data (and it also depends on the use case)
+# https://github.com/cleanlab/cleanlab
+# https://github.com/data-prep-kit/data-prep-kit
+# Handling Missing Values: This is one of the most frequent issues. You can deal with missing data by:
+# Imputation: Filling in missing values with a substitute, such as the mean, median, or mode of the column.
+# Deletion: Removing rows or columns that have too many missing values.
+# Prediction: Using a machine learning model to predict the missing values based on other data points.
+# Removing Duplicates: Identifying and deleting identical records that can skew your analysis.
+# Standardizing and Formatting Data: Ensuring consistency in how data is represented. This includes:
+# Converting all text to a single case (e.g., lowercase).
+# Standardizing date formats (e.g., YYYY-MM-DD).
+# Correcting typos and inconsistent entries (e.g., "New York," "NY," and "NYC" should all be standardized to one format).
+# Correcting Inaccurate Data: Finding and fixing data that is logically incorrect. For example, a person's age cannot be 200, or a price cannot be a negative number. This often involves setting rules or using validation checks
